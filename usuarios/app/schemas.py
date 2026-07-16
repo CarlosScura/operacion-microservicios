15510d4a7e5f3fcd2ce625584ef5b810a82f6fe3
Federico Alarcón Scura <carlos.a.scura@gmail.com>
2026-07-16 20:08:03 -0300

    Feat: Se completaron los schemas del microservicio de usuarios.

---
+++ usuarios/app/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+# GET /usuarios
+class UsuarioResponse(BaseModel):
+    id: int
+    mail: str
+
+# POST /usuarios
+class UsuarioCreate(BaseModel):
+    mail: str
+    password: str
+
+# POST /login
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+
+# GET /usuarios/{id}/tarjetas
+class TarjetaResponse(BaseModel):
+    id: int
+    usuario_id: int
+    nombre: str
+    ultimos_4_digitos: str
+    marca: str
+
+# POST /usuarios/{id}/tarjetas
+class TarjetaCreate(BaseModel):
+    nombre: str
+    ultimos_4_digitos: str
+    marca: str
